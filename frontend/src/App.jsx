30f93586ebbc40ca42fc9facba269f4fa03c469c
binguliki <blikith86@gmail.com>
2025-06-21 09:34:35 +0530

    Create project structure

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className="h-screen flex justify-center items-center">
+        <h2 className="text-5xl">Frontend App</h2>
+      </div>
+    </>
+  )
+}
+
+export default App
